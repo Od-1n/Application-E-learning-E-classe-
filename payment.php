db7ffc6c1f41c731e39fcb97272d1db38626c700
odin789 <m.melmezou456fe@gmail.com>
2022-01-28 01:55:17 +0100

    new update

--- payment.php
+++ payment.php
@@ -64,7 +64,7 @@
   <!-- Bootstrap core JS-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Core theme JS-->
-  <script src="side.js"></script>
+  <script src="side/side.js"></script>
 </body>
 
 </html>
